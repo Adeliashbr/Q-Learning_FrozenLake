ed46062dc60af28fd2174cc4bf546745ad79923c
Adelia <160410664+Adeliashbr@users.noreply.github.com>
2025-05-16 01:31:24 +0700

    FrozenLake.py

--- data.py
+++ data.py
@@ -2,14 +2,14 @@ import gymnasium as gym
 import numpy as np
 import time
 
-env = gym.make("FrozenLake-v1", is_slippery=False)
+env = gym.make("FrozenLake-v1", is_slippery=False, render_mode="human")
 q = np.zeros((env.observation_space.n,env.action_space.n))
 
 alpha = 0.8
 gamma = 0.95
 eps = 1.0
 
-for episode in range (1000):
+for episode in range (5000):
     state,_ = env.reset()
     done = False
     total_reward = 0
